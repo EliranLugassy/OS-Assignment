c79529c3e1118083413880355a0fc9f13dbe6ac2
Eliran Lugassy <eliranlugassy@gmail.com>
2015-11-09 01:02:46 +0200

    added probability function to clinet class

--- Client.java
+++ Client.java
@@ -1,9 +1,13 @@
 import java.io.DataInputStream;	
 import java.io.DataOutput;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Random;
 
 /**
  * 
@@ -25,17 +29,17 @@ public class Client extends Thread{
 
 
 
-/**
- * Constructor who get an args
- * @param R - this number indicates the end of the range. [1,R] (include them)
- * @param fileName - this is the name of file who contains the Probabilities to number from the range
- */
+	/**
+	 * Constructor who get an args
+	 * @param R - this number indicates the end of the range. [1,R] (include them)
+	 * @param fileName - this is the name of file who contains the Probabilities to number from the range
+	 */
 	public Client(int R, String fileName) {
 		_R = R;
 		_fileName = fileName;	
 	}
 
-	
+
 	public void run(){
 
 		try {
@@ -47,9 +51,10 @@ public class Client extends Thread{
 			while(true){
 
 				//rand X+Px - need to find the algo.
-				int x = (int)Math.random()*(_R) + 1;
+				int x = randomX(); //if x get 0 value -> the read from file failed
 				System.out.println("Client "+this.getName()+": sending x");
-				_send_to_server.writeInt(x);
+				_send_to_server.writeInt(x);// wait for responding of the server
+				//when get the response we need to print "Client : got reply y for query  x"
 
 				try {
 					wait();
@@ -65,4 +70,53 @@ public class Client extends Thread{
 			e.printStackTrace();
 		}
 	}
+
+	// [      ]
+	private int randomX() {
+
+
+		try {
+
+			FileInputStream fis = new FileInputStream(_fileName);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			int [] probArr = new int[1000];
+			double prob = 0;
+			int rowInFile=1;
+			int indexOnProbArr=0;
+
+			while(rowInFile<=_R){ // 1 to R (include)
+				prob = ois.readDouble(); // 0.2 // i=1 
+				prob *= 1000; // to get performance number
+				while(indexOnProbArr<prob){
+					probArr[indexOnProbArr++] = rowInFile;
+				}
+				rowInFile++;
+			}
+			ois.close();
+			fis.close();
+
+			Random xQuery = new Random();
+			int chooseX = xQuery.nextInt(1000);
+			return probArr[chooseX];
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+
+		return 0;
+	}
+
+
+
+public static void main(String[] args) {
+	
+}
+
 }
--- Testings.java
+++ Testings.java
@@ -1,13 +1,76 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.StringReader;
+import java.util.Random;
+
 
 public class Testings {
 
-	
+	static int _R=3;
+	static String _fileName = "C:/Users/Eliran Lugassy/Desktop/a.txt";
+
 	public static void main(String[] args) {
-	
-		double a =  Math.random();
-		
-		System.out.println(String.format("%.3f", a));
-		
+
+
+
+
+		try {
+
+			//			FileInputStream fis = new FileInputStream(_fileName);
+			//ObjectInputStream ois = new ObjectInputStream(fis);
+			FileReader fr = new FileReader(_fileName);
+			BufferedReader br = new BufferedReader(fr);
+			int [] probArr = new int[1000];
+			double prob = 0;
+			int rowInFile=1;
+			int indexOnProbArr=0;
+
+
+			while(rowInFile<=_R){ // 1 to R (include)
+				
+				prob = Double.parseDouble(br.readLine());
+				
+				//				prob = fis.; // 0.2 // i=1 // -1 -> end
+				if(prob == -1){
+					break;
+				}
+				prob *= 1000; // to get performance number
+				int k=0;
+				while(k<prob){
+					probArr[indexOnProbArr] = rowInFile;
+					indexOnProbArr++;
+					k++;
+				}
+				rowInFile++;
+			}
+			//			ois.close();
+			//			fis.close();
+			br.close();
+			fr.close();
+
+			Random xQuery = new Random();
+			int chooseX = xQuery.nextInt(1000);
+
+			System.out.println(probArr[chooseX]);
+
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+
+
 	}
-	
+
 }
