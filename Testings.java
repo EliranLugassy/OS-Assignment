3d2320ad552846f5d4c9e5705e054affb4a79553
Eliran Lugassy <eliranlugassy@gmail.com>
2015-12-23 02:02:10 +0200

    fix bugs and added semaphores

--- Testings.java
+++ Testings.java
@@ -1,19 +1,26 @@
+import java.util.concurrent.Semaphore;
+
 import server.Server;
+import sun.awt.Mutex;
 import client.Client;
 
-	//run - function
-	//start - run as a Thread and call to run
+//run - function
+//start - run as a Thread and call to run
 public class Testings {
 
-	
+
 	public static void main(String[] args) {
-		
-		Server myServer = new Server(10, 52333);
-		Client myClient = new Client(3, "C:/Users/Eliran Lugassy/Desktop/prob.txt");
+
+		int port=52334;
+		Semaphore m = new Semaphore(0);
+		m.release(2);
+
+		Server myServer = new Server(10, port, m);
 		myServer.start();
+		Client myClient = new Client(3, "C:/Users/Eliran Lugassy/Desktop/prob.txt", 52334, m);
 		myClient.start();
-		
-		
+
+
 	}
 
 }
